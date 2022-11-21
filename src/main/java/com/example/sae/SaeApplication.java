66f9162160aff11da871a6b0d7c9bdef5c4179b3
Matthéo PERELLE <mattheo.perelle@outlook.fr>
2022-11-21 11:18:22 +0100

    Initial commit

---
+++ src/main/java/com/example/sae/SaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sae/SaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
