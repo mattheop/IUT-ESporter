3f8a312ff566bb03e998ad58eb743df89a893c94
Xzefir <Evan64g@gmail.com>
2022-11-24 12:31:08 +0100

    fix controller

---
+++ src/main/java/com/example/sae/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.sae.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    public HomeController() {
+
+    }
+
+    @GetMapping
+    public String home(Model model) {
+        return "home/home";
+    }
+
+}
